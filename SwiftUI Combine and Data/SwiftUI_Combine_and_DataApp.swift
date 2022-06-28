a8421e073b86ddaf850732e28bc9e84993645284
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-06-28 05:13:33 -0300

    Initial Commit

---
+++ SwiftUI Combine and Data/SwiftUI_Combine_and_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_and_DataApp.swift
+//  SwiftUI Combine and Data
+//
+//  Created by João Carlos Magalhães on 28/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_and_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
